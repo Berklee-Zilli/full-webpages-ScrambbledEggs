f6f61d66e5a643c47ab9a2bf901700d1d33bfc7c
Sam Underhill <sam@Sams-MacBook-Pro-4.local>
2024-11-19 20:06:44 -0500

    level 1 completed

---
+++ task1/using-js-to-make-changes.js
@@ -0,0 +1,5 @@
+document.title = "taskdone by Sam (studnet not professor)";
+document.querySelector("header h1").textContent = 'Task 1';
+document.querySelector('header p').textContent = 'I wear glasses, Share a name with the professor, and prefer python over HTML';
+document.querySelector('Section h2').textContent = 'list Of things I like';
+//document.getElementsByClassName('List-item')//
